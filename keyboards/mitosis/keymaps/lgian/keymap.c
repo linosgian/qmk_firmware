e36029bd1a7f77d9ec4e91b559c5d1ca2029bc9a
Linos Giannopoulos <14967949+linosgian@users.noreply.github.com>
2020-02-25 00:52:06 +0200

    Move arrow keys to the third layer
    Testing whether having all symbol on the 2nd layer is better.

--- keyboards/mitosis/keymaps/lgian/keymap.c
+++ keyboards/mitosis/keymaps/lgian/keymap.c
@@ -32,39 +32,39 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *  q    w      e      r      t        ||        y     u    i     o     p
    *  a    s      d      f      g        ||        h     j    k     l     ;
    *  z    x      c      v      b        ||        n     m    ,     .     /?
-   *        `     FΝ2   esc    ctrl      ||        tab    enter     =    '
-   *       c+sh  shift  alt    bksp      ||        space  FN1    shift   \
+   *       nop    \     esc    ctrl      ||        tab    enter     =    nop
+   *       c+sh  shift  alt    bksp      ||        space  FN1      FN2   shift
    */
   [_xQ] = LAYOUT(
       KC_Q,    KC_W,            KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,      KC_O,     KC_P,
       KC_A,    KC_S,            KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,      KC_L,     KC_SCLN,
       KC_Z,    KC_X,            KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM,   KC_DOT,   KC_SLSH,
-      /*,   */ KC_GRV,          MO(_xF), KC_ESC,  KC_LCTL,     KC_TAB,  KC_ENT,  KC_EQL,   KC_QUOT,
-      /*,   */ LCTL(KC_LSFT),   KC_LSFT, KC_LALT, KC_BSPC,     KC_SPC,  MO(_xN), KC_LSFT,   KC_BSLS),
+      /*,   */ KC_NO,           KC_BSLS, KC_ESC,  KC_LCTL,     KC_TAB,  KC_ENT,  KC_EQL,   KC_QUOT,
+      /*,   */ LCTL(KC_LSFT),   KC_LSFT, KC_LALT, KC_BSPC,     KC_SPC,  MO(_xN), MO(_xF),  KC_LSFT),
   /*
-   *  !     @      up     _     -        ||     +    7     8     9    *
-   *  #    left   down  right   $        ||     "    4     5     6    |
+   *  !     @      ^     _      -        ||     +    7     8     9    *
+   *  #     %      `     ~      $        ||     "    4     5     6    |
    *  []    ()     {}    <>     &        ||     '    1     2     3    0
-   *        `     FΝ2   esc    ctrl      ||     tab    enter     =    '
-   *       c+sh  shift  alt    bksp      ||     space  FN1    shift   \
+   *       nop    \     esc    ctrl      ||        tab    enter     =    nop
+   *       c+sh  shift  alt    bksp      ||        space  FN1      FN2   shift
    */
   [_xN] = LAYOUT(
-      KC_EXLM,          KC_AT,              KC_UP,              KC_UNDS,        KC_MINS,    KC_PLUS, KC_7,    KC_8,    KC_9,     KC_ASTR,
-      KC_HASH,          KC_LEFT,            KC_DOWN,            KC_RIGHT,       KC_DLR,     KC_DQUO, KC_4,    KC_5,    KC_6,     KC_PIPE,
+      KC_EXLM,          KC_AT,              KC_CIRC,            KC_UNDS,        KC_MINS,    KC_PLUS, KC_7,    KC_8,    KC_9,     KC_ASTR,
+      KC_HASH,          KC_PERC,            KC_GRV,             KC_TILD,        KC_DLR,     KC_DQUO, KC_4,    KC_5,    KC_6,     KC_PIPE,
       TD(TD_LCBR_RCBR), TD(TD_LPRN_RPRN),   TD(TD_LBRC_RBRC),   TD(TD_LT_GT),   KC_AMPR,    KC_QUOT, KC_1,    KC_2,    KC_3,     KC_0,
       /*,   */ _______, _______, _______, _______,     _______, _______, _______, _______,
       /*,   */ _______, _______, _______, _______,     _______, _______, _______, _______),
   /*
-   *  ~     ^     pgup   =     pscr      ||     vol+    F7    F8    F9    F10
-   *  %     `     pgdo   \     lang      ||     vol-    F4    F5    F6    F11
-   *  lock nop    nop   nop    nop       ||     mute    F1    F2    F3    F12
-   *        `     FΝ2   esc    ctrl      ||     tab    enter     %    ^
-   *       c+sh  shift  alt    bksp      ||     space  FN1    shift   ~
+   *  nop   pgup  up   pgdown  pscr      ||     vol+    F7    F8    F9    F10
+   *  nop   left  down right   lang      ||     vol-    F4    F5    F6    F11
+   *  lock  nop   nop   nop    nop       ||     mute    F1    F2    F3    F12
+   *       nop    \     esc    ctrl      ||        tab    enter     =    nop
+   *       c+sh  shift  alt    bksp      ||        space  FN1      FN2   shift
    */
   [_xF] = LAYOUT(
-      KC_TILD, KC_CIRC,   KC_PGUP,   KC_EQL,  TD(TD_PSCR_PSCRW),     KC_VOLU, KC_F7,   KC_F8,   KC_F9,    KC_F10,
-      KC_PERC, KC_GRV,    KC_PGDOWN, KC_BSLS, RGUI(KC_SPC),      KC_VOLD, KC_F4,   KC_F5,   KC_F6,    KC_F11,
-      RGUI(KC_L), _______,   _______, _______,  _______,     KC_MUTE, KC_F1,   KC_F2,   KC_F3,    KC_F12,
+      KC_BSLS,      KC_PGUP,   KC_UP,   KC_PGDOWN,  TD(TD_PSCR_PSCRW), KC_VOLU, KC_F7,   KC_F8,   KC_F9,    KC_F10,
+      KC_EQL,       KC_LEFT,   KC_DOWN, KC_RIGHT,   RGUI(KC_SPC),      KC_VOLD, KC_F4,   KC_F5,   KC_F6,    KC_F11,
+      RGUI(KC_L),  _______,    _______, _______,    _______,           KC_MUTE, KC_F1,   KC_F2,   KC_F3,    KC_F12,
       /*,   */ _______,   _______, _______, _______,     _______, _______, _______, _______,
       /*,   */ _______,   _______, _______, _______,     _______, _______, _______, _______),
 };
