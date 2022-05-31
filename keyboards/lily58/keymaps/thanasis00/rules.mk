e1c1d9ba1ef1c29a386633c6ae2d1bae8cf9f91d
Thanasis Katsadas <kathan@admin.grnet.gr>
2022-06-01 02:09:41 +0300

    Add lily58 default layout

---
+++ keyboards/lily58/keymaps/thanasis00/rules.mk
@@ -0,0 +1,5 @@
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB light
+LTO_ENABLE = yes           # significantly reduce the compiled size, but disable the legacy TMK Macros and Functions features
+EXTRAKEY_ENABLE = yes
+OLED_ENABLE = yes
