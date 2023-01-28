b7b0a9e2a7648554d4f08ba7b6bc46f84d85ec54
Linos Giannopoulos <linosgian00@gmail.com>
2023-01-28 03:51:42 +0200

    Add custom lily58l keymap

--- keyboards/lily58/keymaps/lily58l/keymap.c
+++ keyboards/lily58/keymaps/lily58l/keymap.c
@@ -49,11 +49,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-  KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                             KC_LALT, KC_LGUI,LOWER, KC_SPC,   KC_ENT,   RAISE,   KC_BSPC, KC_RGUI
+  _______, _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,
+  _______,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______,
+  KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ESC,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,  KC_MPLY,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  _______,
+                             KC_LALT, KC_LSFT,RAISE, KC_BSPC,   KC_SPC,   LOWER,   KC_ENT, _______
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -71,10 +71,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
-  _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-                             _______, _______, _______, _______, _______,  _______, _______, _______
+  _______, KC_EXLM,   KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                     KC_LPRN,   KC_7,   KC_8, KC_9,  KC_RPRN,  _______,
+  KC_LABK, KC_AMPR, KC_ASTR,   KC_QUOT, KC_DQUO, KC_UNDS,                   KC_LCBR, KC_4, KC_5, KC_6, KC_RCBR, KC_RABK,
+  _______, KC_GRV, KC_CIRC, KC_PLUS, KC_MINS, KC_PIPE, _______, _______, KC_LBRC, KC_1, KC_2, KC_3, KC_RBRC, _______,
+                             KC_DOT, KC_EQL, KC_0, _______, _______,  _______, _______, _______
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -93,10 +93,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_RAISE] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-  KC_F1,  KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,                       XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,
-  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   _______, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-                             _______, _______, _______,  _______, _______,  _______, _______, _______
+  _______,  RGB_TOG,  KC_PGUP,    KC_UP,    KC_PGDN,    _______,                     _______,   LCTL(KC_TAB),    LSFT(LCTL(KC_PGUP)),    _______,    _______,    _______,
+  _______,  _______,    KC_LEFT,   KC_DOWN,   KC_RIGHT,   _______,                   LGUI(KC_SPC), LSFT(LCTL(KC_TAB)), LSFT(LCTL(KC_PGDN)), _______,   _______, _______,
+  _______,   _______,   _______,   _______,   LCTL(LSFT(KC_V)),  _______,   _______, _______,  _______, _______, _______,  _______, _______, _______,
+                             _______, _______, _______,  _______, KC_TAB,  KC_TILDE, KC_BSLS, _______
 ),
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -266,9 +266,6 @@ void render_status_main(void) {
     oled_write_P(PSTR("-----"), false);
     // Show modifier status
     render_mod_status(get_mods());
-    // Add a empty line
-    oled_write_P(PSTR("-----"), false);
-    render_keylogger_status();
 }
 
 bool oled_task_user(void) {
@@ -304,9 +301,9 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     }
     else {
       if (clockwise) {
-          tap_code(KC_VOLU);
-      } else {
           tap_code(KC_VOLD);
+      } else {
+          tap_code(KC_VOLU);
       }
     }
   }
@@ -321,9 +318,9 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     }
     else {
       if (clockwise) {
-          tap_code(KC_DOWN);
+          tap_code(KC_MNXT);
       } else {
-          tap_code(KC_UP);
+          tap_code(KC_MPRV);
       }
     }
   }
