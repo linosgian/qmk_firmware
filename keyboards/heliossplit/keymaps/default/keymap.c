603cb34b9b599b418cba65280c9a621f9b3e577d
Linos Giannopoulos <linosgian00@gmail.com>
2025-02-05 01:58:55 +0200

    cc

--- keyboards/heliossplit/keymaps/default/keymap.c
+++ keyboards/heliossplit/keymaps/default/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,              KC_Y,              KC_U,           KC_I,          KC_O,      KC_P,
 RALT_T(KC_A),      KC_S,              LCTL_T(KC_D),      LSFT_T(KC_F),      KC_G,              KC_H,              RSFT_T(KC_J),   LCTL_T(KC_K),  KC_L,      RALT_T(KC_SEMICOLON),
 KC_Z,              KC_X,              KC_C,              KC_V,              KC_B,              KC_N,              KC_M,           KC_COMM,       KC_DOT,    KC_SLSH,
-                   CW_TOGG,           LT(2, KC_ESC),     KC_BSPC,           KC_MUTE,           KC_SPC,            LT(1, KC_ENT),  LT(3, CW_TOGG)
+                   CW_TOGG,           LT(2, KC_ESC),     KC_BSPC,           KC_MUTE,           KC_SPC,            LT(1, KC_ENT),  LT(3, LGUI(KC_SPC))
 ),
 /* 1
  * ┌──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┐
@@ -39,8 +39,8 @@ KC_LBRC,           KC_1,              KC_2,              KC_3,              KC_R
 ),
 
 [2] = LAYOUT(
-KC_NO,            KC_PGDN,              KC_UP,              KC_PGUP,       KC_NO,             _______,            LCTL(KC_TAB),       LSFT(LCTL(KC_PGUP)), _______,    _______,
-KC_NO,            KC_LEFT,              KC_DOWN,            KC_RIGHT,      KC_NO,             LGUI(KC_SPC),       LSFT(LCTL(KC_TAB)), LSFT(LCTL(KC_PGDN)), _______,    _______,
+KC_NO,            KC_PGDN,              KC_UP,              KC_PGUP,       KC_NO,             _______,            LCTL(KC_TAB),       _______, LSFT(LCTL(KC_PGUP)),    _______,
+KC_NO,            KC_LEFT,              KC_DOWN,            KC_RIGHT,      KC_NO,             LGUI(KC_SPC),       LSFT(LCTL(KC_TAB)), LCTL, LSFT(LCTL(KC_PGDN)),    _______,
 KC_NO,            _______,              _______,            _______,       KC_NO,             KC_NO,              KC_NO,              KC_NO,               KC_NO,     KC_NO,
 KC_NO,            _______,              _______,            KC_MPLY,       KC_TAB,            KC_NO,              KC_NO
 ),
